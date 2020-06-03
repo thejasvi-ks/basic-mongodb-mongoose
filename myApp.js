b992fada2b35886ddfb40ca1690ea8aeb49bf50c
Glitch (hilarious-noiseless-bell) <none>
2020-06-03 17:15:23 +0000

    🌞🎟 Checkpoint ./myApp.js:16834942/338

--- myApp.js
+++ myApp.js
@@ -15,27 +15,23 @@
 
 // var Person = mongoose.model('Person', PersonSchema);
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI)
 
 /** 2) Create a 'Person' Model */
-var personSchema = new mongoose.Schema({
+const personSchema = new mongoose.Schema({
   name: String,
   age: Number,
   favoriteFoods: [String]
-});
+})
 
 /** 3) Create and Save a Person */
-var Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema)
 
-var createAndSavePerson = function(done) {
-  var janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["vodka", "air"]});
+const createAndSavePerson = function(done) {
+  const janeDoe = new Person({name:'Jane Doe',age:25,favoriteFoods:['Bread','Pizza','Pasta']})
 
-  janeFonda.save(function(err, data) {
-    if (err) return console.error(err);
-
-    done(null, data)
-  });
+  janeDoe.save((err, data) => err ? done(err) : done(null,data));
 };
 
 
