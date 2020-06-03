2c41065ac22424f20c19b3dab5af04658e2c486e
Glitch (hilarious-noiseless-bell) <none>
2020-06-03 16:21:06 +0000

    🔖🍏 Checkpoint ./myApp.js:16834942/264

--- myApp.js
+++ myApp.js
@@ -16,6 +16,8 @@
 
 const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 /** # SCHEMAS and MODELS #
@@ -43,8 +45,15 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // <Your code here >
 
-var Person /* = <Your Model> */
+ /* = <Your Model> */
 
+var Person = new Schema({
+  
+  name : {type : String, required: true},
+  age : Number,
+  favoriteFoods : [String]
+  
+});
 // **Note**: Glitch is a real server, and in real servers interactions with
 // the db are placed in handler functions, to be called when some event happens
 // (e.g. someone hits an endpoint on your API). We'll follow the same approach
